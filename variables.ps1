d2559e6179c0e552321b4efb9dc12520e7a79543
Stone <info@simracingjustfair.org>
2024-07-21 15:25:10 +0200

    variables source

---
+++ variables.ps1
@@ -0,0 +1,8 @@
+# where is rfactor 2 located (currently program files (x86) is not supported)
+#$RF2ROOT="c:\program files (x86)\steam\steamapps\common\rfactor 2"
+$RF2ROOT="$HOME\rf2ds"
+
+# automatic SteamUpload, change ALL parameters
+$STEAMUPLOAD="false"
+$STEAMUSER="changeme"
+$STEAMPASSWORD="changeme"

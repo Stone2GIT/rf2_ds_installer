def05cd6c0ab8bc2fefd18c0ada0c0b9e6ebb22c
Stone <info@simracingjustfair.org>
2024-07-21 15:42:42 +0200

    download and install rf2ds

--- rf2_ds_installer.ps1
+++ rf2_ds_installer.ps1
@@ -1,4 +1,4 @@
-#
+ #
 # simple script to install and base configure rFactor 2 dedicated server
 
 # Stone, 07/2024, info@simracingjustfair.org
@@ -9,6 +9,11 @@
 # source variables
 . ./variables.ps1
 
+if (-not (Test-Path "$RF2ROOT")) {
+ mkdir $RF2ROOT
+ mkdir $RF2ROOT\SteamCMD
+}
+
 $CURRENTVERSION=(Get-Date -Format "yyyy.MM.dd")
 $CURRENTLOCATION=((Get-Location).Path)
 
@@ -17,5 +22,9 @@ $ARGUMENTS="Invoke-RestMethod -Uri https://steamcdn-a.akamaihd.net/client/instal
 start-process -FilePath powershell -ArgumentList $ARGUMENTS -NoNewWindow -Wait
 
 # extract SteamCMD
-$ARGUMENTS="Expand-Archive -Force $STEAMINSTALLDIR\steamcmd.zip -DestinationPath ""$RF2ROOT\SteamCMD""""
+$ARGUMENTS="Expand-Archive -Force $RF2ROOT\SteamCMD\steamcmd.zip -DestinationPath ""$RF2ROOT\SteamCMD"""
 start-process -FilePath powershell -ArgumentList $ARGUMENTS -NoNewWindow -Wait
+
+# download and install rFactor 2 dedicated server
+$ARGUMENTS="+force_install_dir $RF2ROOT +login anonymous +app_update 400300 +quit"
+start-process -FilePath "$RF2ROOT\SteamCMD\steamcmd.exe" -ArgumentList $ARGUMENTS -NoNewWindow -Wait
